8ac8f91a815d8f97cea4a846e8b4d8fcaacfa295
Nawin chander <nawinnapzy289@gmail.com>
2026-04-18 21:21:09 +0530

    add

--- Simple JS CQRS.js
+++ Simple JS CQRS.js
@@ -1,3 +1,18 @@
 //// Simple JS CQRS Example
 /// Without CQRS
 
+class UserService {
+  constructor() {
+    this.users = [];
+  }
+
+  createUser(user) {
+    this.users.push(user);
+  }
+
+  getUser(id) {
+    return this.users.find(u => u.id === id);
+  }
+}
+
+
